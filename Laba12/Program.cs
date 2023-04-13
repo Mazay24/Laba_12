7ec15bb19e02af5b5f319b1f8bc415744c84a1b9
Исаев Никита <96546414+Mazay24@users.noreply.github.com>
2023-04-13 12:06:18 +0300

    Add files via upload

---
+++ Laba12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Laba12
+{
+    public delegate void Delegat(string text);
+    class One
+    {
+        public event Delegat Event;
+
+        public void Generate(string eventName)
+        {
+            Event.Invoke(eventName);
+        }
+    }
+
+    class Two
+    {
+        public void HandleEvent(string eventName)
+        {
+            Console.WriteLine($"Событие сгенерировано объектом: {eventName}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            One one1 = new One();
+            One one2 = new One();
+            Two two1 = new Two();
+
+            one1.Event += two1.HandleEvent;
+            one2.Event += two1.HandleEvent;
+
+            one1.Generate("Первый объект");
+            one2.Generate("Второй объект");
+
+        }
+    }
+}
